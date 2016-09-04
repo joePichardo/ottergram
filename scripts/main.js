c050375262e07969f7fd80c18d89b51aa959530a
joePichardo <joepichar@gmail.com>
2016-09-04 10:07:39 -0700

    Added script that would hide thumbnails on esc key press

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
+
 
 function setDetails(imageUrl, titleText) {
   'use strict'; // use strict lets browser know it conforms to the most recent standard version of javascript
@@ -42,10 +45,27 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY){
+      hideDetails();
+    }
+  });
+}
+
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
