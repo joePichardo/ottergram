71e57113830efd7de2820e9fbb88264b7c43f214
joePichardo <joepichar@gmail.com>
2016-09-04 11:00:45 -0700

    description Added styles, index element, and js to add animations/transitions to elements on the page. Hovering over a thumbnail scales up the size, clicking on the item puts it in the main image with a zoom transition.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
 
@@ -53,7 +55,14 @@ function hideDetails() {
 
 function showDetails() {
   'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  /* delays function to prevent immediate change, helps show animation */
+  setTimeout(function(){
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+
 }
 function addKeyPressHandler() {
   'use strict';
